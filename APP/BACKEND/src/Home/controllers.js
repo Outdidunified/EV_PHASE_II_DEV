df31077e87fadc5d5f528012eb01b57fa789f383
vivek-outdid <vivek@outdidtech.com>
2024-09-20 18:08:01 +0530

    APP SERVER - case-insensitive is not suitable for our application for some reason, if we implement this, we have to apply this to all API's

--- APP/BACKEND/src/Home/controllers.js
+++ APP/BACKEND/src/Home/controllers.js
@@ -11,7 +11,7 @@ async function searchCharger(req, res) {
         const userCollection = db.collection('users');
         const socketGunConfigCollection = db.collection('socket_gun_config');
 
-        const chargerDetails = await evDetailsCollection.findOne({ charger_id: { $regex: new RegExp(`^${ChargerID}$`, 'i') },  status: true });
+        const chargerDetails = await evDetailsCollection.findOne({ charger_id: ChargerID,  status: true });
 
         // if(chargerDetails.assigned_association_id === null){
         //     const errorMessage = 'Device ID not found !';
@@ -29,7 +29,7 @@ async function searchCharger(req, res) {
         if(userDetails){
             // check user status true or false
             if(userDetails.status === true){
-                const socketGunConfig = await socketGunConfigCollection.findOne({ charger_id: { $regex: new RegExp(`^${ChargerID}$`, 'i') } }); // fetch total connector id's
+                const socketGunConfig = await socketGunConfigCollection.findOne({ charger_id: ChargerID }); // fetch total connector id's
                 //check assigned association id is null value in users 
                 if(userDetails.assigned_association === null){
                     //check charger is in public or private 1 or 2
